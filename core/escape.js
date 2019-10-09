146e98e2408327d7a264e7a3a184439d1aa4fdba
cao <kum_cp@yahoo.com.vn>
2019-10-09 14:49:39 +0900

    update some bugs related to escape

--- core/escape.js
+++ core/escape.js
@@ -1,10 +1,14 @@
+const escapeNewLine = val => val.replace(/[\n\r]/g, '');
+
 /**
  * Escape value in CSV (Which has comma mark)
  * @param {*} val
  */
-const escapeValueCSV = val => (`${val}`.indexOf(',') >= 0 ? `"${val}"` : val);
-
-const escapeNewLine = val => val.replace(/[\n\r]/g, '');
+const escapeValueCSV = (val, escapeValues = [',']) =>
+    (escapeValues.map(item => `${val}`.indexOf(item) >= 0).filter(containSpecialCharacter => containSpecialCharacter)
+        .length > 0
+        ? escapeNewLine(`"${val}"`)
+        : val);
 
 module.exports = {
     escapeValueCSV,
--- core/transformObject/objectTransform.js
+++ core/transformObject/objectTransform.js
@@ -1,17 +1,11 @@
 /**
  * Filter key value in a object
  * @param {*} object All object
- * @param {(key, val) =>{}} filter filter function
- *
- * @throws 'OBJECT_NOT_DEFINED', 'NOT_AN_OBJECT', 'FILTER_IS_NOT_FUNCTION'
+ * @param {*} filter filter function
  */
-const objectFilter = (object, filter) => {
-    if (!object) throw new Error('OBJECT_NOT_DEFINED');
-    if (typeof (object) !== 'object') throw new Error('NOT_AN_OBJECT');
-    if (typeof (filter) !== 'function') throw new Error('FILTER_IS_NOT_FUNCTION');
-
-    return Object.keys(object)
-        .filter(key => (filter || ((k, v) => v))(key, object[key]))
+const objectFilter = (object, filter) =>
+    Object.keys(object)
+        .filter((key) => filter(key, object[key]))
         .reduce(
             (newObj, key) => ({
                 ...newObj,
@@ -19,7 +13,7 @@ const objectFilter = (object, filter) => {
             }),
             {}
         );
-};
+
 /**
  * Filter object with a list of key (return a new object)
  *
@@ -27,7 +21,7 @@ const objectFilter = (object, filter) => {
  * @param {*} keyList list key want to filter
  */
 const objectFilterByKeys = (object, keyList) =>
-    objectFilter(object, key => keyList.indexOf(key) > -1);
+    objectFilter(object, (key) => keyList.indexOf(key) > -1);
 
 /**
  * Turn list object in hierachy into a flatten list
@@ -48,6 +42,12 @@ const objectFilterByKeys = (object, keyList) =>
  *          { param1_3: value2_3 },
  *      ]
  * }
+ * -> [
+ *  {param1: value1, param1_1: value1_1},
+ *  {param1: value1, param1_2: value1_2},
+ *  {param1: value1, param1_4: value1_3},
+ *  ...
+ * ]
  *
  * @param {*} chilrenObjectList
  *
@@ -57,28 +57,38 @@ const objectFilterByKeys = (object, keyList) =>
  * @returns {Array} a list contain both the children object and parent object data
  */
 const flattenChildrenObjectList = (chilrenObjectList, childrenField) =>
-    chilrenObjectList
-        .filter(object => (!object ? {} : object))
-        .reduce((flattenList, object) => {
-            if (Array.isArray(object[childrenField])) {
-                return flattenList.concat(
-                    flattenChildrenObjectList(
-                        object[childrenField],
-                        childrenField
-                    ).map(childObj => ({
-                        ...childObj,
-                        ...objectFilter(object, key => key !== childrenField)
-                    }))
-                );
-            }
-            return [
-                ...flattenList,
-                objectFilter(object, key => key !== childrenField)
-            ];
-        }, []);
+    chilrenObjectList.reduce((flattenList, object) => {
+        if (Array.isArray(object[childrenField])) {
+            return flattenList.concat(
+                flattenChildrenObjectList(
+                    object[childrenField],
+                    childrenField
+                ).map((childObj) => ({
+                    ...childObj,
+                    ...objectFilter(object, (key) => key !== childrenField)
+                }))
+            );
+        }
+        return [
+            ...flattenList,
+            objectFilter(object, (key) => key !== childrenField)
+        ];
+    }, []);
 
 /**
  * Spread nested object with value is object
+ * Ex: const obj = {
+ *     a: 3,
+ *     b: 4,
+ *     c: {
+ *         d:5,
+ *         e:6,
+ *         b:7
+ *     }
+ * }
+ *
+ * spreadNestedObject(obj) // => {a: 3, b: 7, d: 5, e: 6}
+ *
  *
  * @param {*} object parent object
  * @param {*} fieldList child fields which are object to be exploit
@@ -98,7 +108,6 @@ const spreadNestedObject = (object, fieldList) =>
     }, {});
 
 module.exports = {
-    objectFilter,
     flattenChildrenObjectList,
     objectFilterByKeys,
     spreadNestedObject
