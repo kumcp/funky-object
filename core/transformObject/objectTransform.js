a05feb118bb680725416e7fe03ca8f3d43568c67
cao <kum_cp@yahoo.com.vn>
2019-10-18 15:16:25 +0900

    add circle ci

--- core/transformObject/objectTransform.js
+++ core/transformObject/objectTransform.js
@@ -112,6 +112,7 @@ const spreadNestedObject = (object, fieldList) =>
     }, {});
 
 module.exports = {
+    objectFilter,
     flattenChildrenObjectList,
     objectFilterByKeys,
     spreadNestedObject
